f25a42b8b02a6d4109af0d6eaacc02b68b71a546
David Crawford <crawford@mail.gvsu.edu>
2023-06-29 23:01:35 -0400

    Added cross reference sidebar that works with mobile

--- src/layouts/components/CrossReference/CrossReference.tsx
+++ src/layouts/components/CrossReference/CrossReference.tsx
@@ -1,7 +1,10 @@
 import { Icon } from '@iconify/react';
 import { SectionHeader } from '../SectionHeader/SectionHeader';
 
-export const CrossReference = () => {
+interface CrossReferenceProps {
+  toggleSidebar: (value: boolean) => void;
+}
+export const CrossReference = (props: CrossReferenceProps) => {
   return (
     <div className="black-text flex h-full flex-col gap-4 bg-stone-50 p-4 @container dark:bg-stone-900">
       <div className="flex flex-row justify-between">
@@ -15,13 +18,26 @@ export const CrossReference = () => {
             className="orange-text text-2xl"
           />
         </div>
-        <Icon icon="heroicons-outline:x-mark" className="text-2xl" />
+        <button onClick={() => props.toggleSidebar(false)}>
+          <Icon icon="heroicons-outline:x-mark" className="text-2xl" />
+        </button>
       </div>
       <div className="flex flex-col gap-4">
         <p className="orange-text font-cinzelFont text-xl font-bold">
           Cross-Reference
         </p>
         <SectionHeader />
+        <p className="font-karla text-lg">
+          The Beatitudes respond to the natural desire for happiness. This
+          desire is of divine origin: God has placed it in the human heart in
+          order to draw man to the One who alone can fulfill it: We all want to
+          live happily; in the whole human race there is no one who does not
+          assent to this proposition, even before it is fully articulated. How
+          is it, then, that I seek you, Lord? Since in seeking you, my God, I
+          seek a happy life, let me seek you so that my soul may live, for my
+          body draws life from my soul and my soul draws life from you. God
+          alone satisfies.
+        </p>
       </div>
     </div>
   );
--- src/layouts/components/WritingContent/WritingContent.tsx
+++ src/layouts/components/WritingContent/WritingContent.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import {
-  Bar,
-  ColumnResizer,
-  Container,
-  Resizer,
-  Section,
-} from '@column-resizer/react';
 import { ContentHeader } from '../ContentParent/ContentHeader';
-import MDXContent from '../MDXContent';
+//import MDXContent from '../MDXContent';
 import { WritingControl } from '../WritingControl/WritingControl';
 import React from 'react';
 import { useWindowSize } from '@/hooks/useWindowSize';
-import { set } from 'date-fns';
 import { CrossReference as CrossReferenceNumeral } from '../Numerals/CrossReference/CrossReference';
 import { CrossReference } from '../CrossReference/CrossReference';
-
-const MIN_SIZE = 320;
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 interface WritingContentProps {
   content: string;
@@ -24,65 +16,67 @@ interface WritingContentProps {
 export const WritingContent = (props: WritingContentProps) => {
   const [width] = useWindowSize();
   const [isSidebarOpen, setIsSidebarOpen] = React.useState<boolean>(false);
+  const isMobile = useMediaQuery(768);
 
-  const columnResizerRef = React.createRef<ColumnResizer>();
-
-  const beforeApplyResizer = (resizer: Resizer): void => {
-    if (resizer.getSectionSize(1) < 300) {
-      resizer.resizeSection(1, { toSize: 0 });
-      setIsSidebarOpen(false);
-    } else {
-      setIsSidebarOpen(true);
-    } /* else if (resizer.getSectionSize(1) < 100) {
-      resizer.resizeSection(1, { toSize: MIN_SIZE });
-    } */
+  const getDefaultSize = (w: number) => {
+    if (w < 1024) {
+      return 33;
+    } else return 20;
   };
 
-  const toggleSidebar = (open: boolean) => {
-    const container = columnResizerRef.current;
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-    if (container) {
-      const resizer = container.getResizer();
-      resizer.resizeSection(1, { toSize: open ? MIN_SIZE : 0 });
-      setIsSidebarOpen(open ? true : false);
-      /* if (resizer.getSectionSize(0) === 0) {
-        resizer.resizeSection(0, { toSize: MIN_SIZE });
-      } */
-
-      container.applyResizer(resizer);
-    }
+  const MainContent = () => {
+    return (
+      <div className="black-text">
+        <ContentHeader />
+        <div className="black-text container gap-4 text-center md:col-10 lg:col-7">
+          <p className="header-text mb-6 text-left">About</p>
+          <button onClick={() => toggleSidebar()}>
+            <CrossReferenceNumeral type="gray">1234</CrossReferenceNumeral>
+          </button>
+          <div className="content text-left font-karla font-normal"></div>
+        </div>
+        <WritingControl />
+      </div>
+    );
   };
 
   if (width === 0) return null;
+  if (isMobile)
+    return (
+      <div className="w-full">
+        {isSidebarOpen ? (
+          <CrossReference toggleSidebar={toggleSidebar} />
+        ) : (
+          <MainContent />
+        )}
+      </div>
+    );
+
   return (
     <div className="w-full">
-      <Container
-        columnResizerRef={columnResizerRef}
-        beforeApplyResizer={beforeApplyResizer}>
-        <Section minSize={320}>
-          <div className="black-text">
-            <ContentHeader />
-            <div className="black-text container gap-4 text-center md:col-10 lg:col-7">
-              <p className="header-text mb-6 text-left">About</p>
-              <button onClick={() => toggleSidebar(true)}>
-                <CrossReferenceNumeral type="gray">1234</CrossReferenceNumeral>
-              </button>
-              <div className="content text-left font-karla font-normal"></div>
-            </div>
-            <WritingControl />
-          </div>
-        </Section>
-        <Bar
-          size={2}
-          onClick={() => toggleSidebar(false)}
-          className={`cursor-col-resize ${
-            isSidebarOpen ? 'bg-stone-300' : 'bg-transparent'
+      <PanelGroup direction="horizontal">
+        <Panel minSize={getDefaultSize(width)}>
+          <MainContent />
+        </Panel>
+        <PanelResizeHandle
+          className={`w-[2px] cursor-col-resize ${
+            isSidebarOpen === true
+              ? 'bg-stone-300 dark:bg-stone-600'
+              : 'bg-transparent'
           }`}
         />
-        <Section minSize={0} defaultSize={1}>
-          <CrossReference />
-        </Section>
-      </Container>
+        {isSidebarOpen ? (
+          <Panel
+            defaultSize={getDefaultSize(width)}
+            minSize={getDefaultSize(width)}>
+            <CrossReference toggleSidebar={toggleSidebar} />
+          </Panel>
+        ) : null}
+      </PanelGroup>
     </div>
   );
 };
